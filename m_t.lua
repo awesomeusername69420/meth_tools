32b0fb92b9b2a442016fb1b848e027131bf5f388
ts03GCZqIsTZtu4 <vampire94937@gmail.com>
2021-06-06 23:34:10 -0400

    Add files via upload

---
+++ m_t.lua
@@ -0,0 +1,348 @@
+--[[
+	Commands:
+
+	m_render_fov (int)				|		Sets FOV
+	m_render_toggle_antiblind		|		Toggles anti ULX blind
+	m_render_toggle_fullbright		|		Toggles fullbright
+	
+	m_tools_gestureloop_set (str)	|		Sets action for gestureloop (ex: "dance")
+	m_tools_psay_message (str)		|		Sets message used for ULX psay spammer
+	m_tools_toggle_gestureloop		|		Toggles gestureloop
+	m_tools_toggle_psay				|		Toggles ULX psay spammer
+]]
+
+--[[
+	Localization
+]]
+
+local cmd = concommand
+local Color = Color
+local debug = debug
+local FindMetaTable = FindMetaTable
+local game = game
+local GetConVar = GetConVar
+local grab = hook
+local ipairs = ipairs
+local LocalPlayer = LocalPlayer
+local math = math
+local MsgC = MsgC
+local pairs = pairs
+local render = render
+local string = string
+local surface = surface
+local table = table
+local timer = timer
+local tostring = tostring
+local util = util
+
+local pt = FindMetaTable("Player")
+local ccmd = FindMetaTable("CUserCmd")
+
+--[[
+	Variables
+]]
+
+local vars = {
+	-- Timers
+
+	["fasttimer"] = tostring(math.random(-2147483648, 2147483647)),
+	["slowtimer"] = tostring(math.random(-2147483648, 2147483647)),
+
+	-- Render
+
+	["fov"] = GetConVar("fov_desired"):GetInt(),
+	["fullbright"] = false,
+
+	-- Tools
+
+	["antiblind"] = false,
+	["gesture"] = "dance",
+	["gestureloop"] = false,
+	["namestore"] = LocalPlayer():Name(),
+	["psay"] = false,
+	["psay_msg"] = "message",
+}
+
+--[[
+	Detours
+]]
+
+local alertDetour = function(evt, data)
+	surface.PlaySound("garrysmod/balloon_pop_cute.wav")
+
+	if data then
+		MsgC(Color(255, 100, 100), "Blocked ", Color(100, 255, 255), tostring(evt) .. " " .. tostring(data), Color(255, 100, 100), ".")
+	else
+		MsgC(Color(255, 100, 100), "Blocked ", Color(100, 255, 255), tostring(evt), Color(255, 100, 100), ".")
+	end
+end
+
+local annoyingtable = {["whats the max tabs you can have open on a vpn"] = {nil}, ["how many vpns does it take to stop a ddos"] = {nil}, ["whats better analog or garrys mod"] = {nil}, ["whats the time"] = {nil}, ["is it possible to make a clock in binary"] = {nil}, ["how many cars can you drive at once"] = {nil}, ["did you know there's more planes on the ground than there is submarines in the air"] = {nil}, ["how many busses can you fit on 1 bus"] = {nil}, ["how many tables does it take to support a chair"] = {nil}, ["how many doors does it take to screw a screw"] = {nil}, ["how long can you hold your eyes closed in bed"] = {nil}, ["how long can you hold your breath for under spagetti"] = {nil}, ["whats the fastest time to deliver the mail as mail man"] = {nil}, ["how many bees does it take to make a wasp make honey"] = {nil}, ["If I paint the sun blue will it turn blue"] = {nil}, ["how many beavers does it take to build a dam"] = {nil}, ["how much wood does it take to build a computer"] = {nil}, ["can i have ur credit card number"] = {nil}, ["is it possible to blink and jump at the same time"] = {nil}, ["did you know that dinosaurs were,  on average,  large"] = {nil}, ["how many thursdays does it take to paint an elephant purple"] = {nil}, ["if cars could talk how fast would they go"] = {nil}, ["did you know theres no oxygen in space"] = {nil}, ["do toilets flush the other way in australia"] = {nil}, ["if i finger paint will i get a splinter"] = {nil}, ["can you build me an ant farm"] = {nil}, ["did you know australia hosts 4 out of 6 of the deadliest spiders in the world"] = {nil}, ["is it possible to ride a bike in space"] = {nil}, ["can i make a movie based around your life"] = {nil}, ["how many pants can you put on while wearing pants"] = {nil}, ["if I paint a car red can it wear pants"] = {nil}, ["how come no matter what colour the liquid is the froth is always white"] = {nil}, ["can a hearse driver drive a corpse in the car pool lane"] = {nil}, ["how come the sun is cold at night"] = {nil}, ["why is it called a TV set when there is only one"] = {nil}, ["if i blend strawberries can i have ur number"] = {nil}, ["if I touch the moon will it be as hot as the sun"] = {nil}, ["did u know ur dad is always older than u"] = {nil}, ["did u know the burger king logo spells burger king"] = {nil}, ["did uknow if u chew on broken glass for a few mins,  it starts to taste like blood"] = {nil}, ["did u know running is faster than walking"] = {nil}, ["did u kno the colur blue is called blue because its blue"] = {nil}, ["did you know a shooting star isnt a star"] = {nil}, ["did u know shooting stars dont actually have guns"] = {nil}, ["did u kno the great wall of china is in china"] = {nil}, ["statistictal fact: 100% of non smokers die"] = {nil}, ["did you kmow if you eat you poop it out"] = {nil}, ["did u know rain clouds r called rain clouds cus they are clouds that rain"] = {nil}, ["if cows drink milk is that cow a cannibal"] = {nil}, ["did u know you cant win a staring contest with a stuffed animal"] = {nil}, ["did u know if a race car is at peak speed and hits someone they'll die"] = {nil}, ["did u know the distance between the sun and earth is the same distance as the distance between the earth and the sun"] = {nil}, ["did u kno flat screen tvs arent flat"] = {nil}, ["did u know aeroplane mode on ur phone doesnt make ur phone fly"] = {nil}, ["did u kno too many britdhays can kill you"] = {nil}, ["did u know rock music isnt for rocks"] = {nil}, ["did u know if you eat enough ice you can stop global warming"] = {nil}, ["if ww2 happened before vietnam would that make vietnam world war 2"] = {nil}, ["did you know 3.14 isn't a real pie"] = {nil}, ["did u know 100% of stair accidents happen on stairs"] = {nil}, ["can vampires get AIDS"] = {nil}, ["what type of bird was a dodo"] = {nil}, ["did u know dog backwards is god"] = {nil}, ["did you know on average a dog barks more than a cat"] = {nil}}
+
+local badcmds = {
+	"+back",
+	"+forward",
+	"+left",
+	"+moveleft",
+	"+right",
+	"+voicerecord",
+	"-back",
+	"-forward",
+	"-left",
+	"-moveleft",
+	"-right",
+	"-voicerecord",
+	"bind",
+	"bind_mac",
+	"bindtoggle",
+	"cl_yawspeed",
+	"connect",
+	"demos",
+	"disconnect",
+	"engine_no_focus_sleep",
+	"impulse",
+	"jpeg",
+	"kill",
+	"mat_texture_limit",
+	"pp_bloom",
+	"pp_bokeh",
+	"pp_dof",
+	"pp_motionblur",
+	"pp_stereoscopy",
+	"pp_texturize",
+	"pp_texturize_scale",
+	"pp_toytown",
+	"record",
+	"retry",
+	"say",
+	"screenshot",
+	"startmovie",
+}
+
+local detours = {
+	clearbuttons = ccmd.ClearButtons,
+	clearmovement = ccmd.ClearMovement,
+	setviewangles = ccmd.SetViewAngles,
+
+	hooktable = hook.GetTable,
+	runconsolecommand = RunConsoleCommand,
+	ptconcommand = pt.ConCommand,
+}
+
+ccmd.SetViewAngles = function(...)
+	if not ... then
+		return
+	end
+
+	local s = string.lower(debug.getinfo(2).short_src)
+
+	if string.find(s, "taunt_camera") then
+		return
+	end
+
+	return detours.setviewangles(...)
+end
+
+ccmd.ClearButtons = function(...)
+	if not ... then
+		return
+	end
+
+	local s = string.lower(debug.getinfo(2).short_src)
+
+	if string.find(s, "taunt_camera") then
+		return
+	end
+
+	return detours.ClearButtons(...)
+end
+
+ccmd.ClearMovement = function(...)
+	if not ... then
+		return
+	end
+
+	local s = string.lower(debug.getinfo(2).short_src)
+
+	if string.find(s, "taunt_camera") then
+		return
+	end
+
+	return detours.ClearMovement(...)
+end
+
+concommand.GetTable = function()
+	alertDetour("concommand.GetTable()")
+
+	return annoyingtable
+end
+
+hook.GetTable = function()
+	alertDetour("hook.GetTable()")
+
+	return annoyingtable
+end
+
+RunConsoleCommand = function(command, ...)
+	for k, v in pairs(badcmds) do
+		if string.find(command, v) then
+			alertDetour("RunConsoleCommand", command .. " " .. tostring(...))
+
+			return
+		end
+	end
+
+	return detours.runconsolecommand(command, ...)
+end
+
+pt.ConCommand = function(command)
+	for _, v in pairs(badcmds) do
+		if string.find(command, v) then
+			alertDetour("ConCommand()", command)
+
+			return 
+		end
+	end
+
+	return detours.ptconcommand(command)
+end
+
+--[[
+	Hooks
+]]
+
+grab.Add("CalcView", tostring({}), function(ply, pos, angles, fov, zn, zf)
+	if not IsValid(ply) then 
+		return
+	end
+
+	local v = ply:GetVehicle()
+	local w = ply:GetActiveWeapon()
+
+	fov = fov + (vars["fov"] - GetConVar("fov_desired"):GetInt())
+
+	local view = {
+		origin = pos,
+		angles = angles,
+		fov = fov,
+		znear = zn,
+		zfar = zf
+	}
+
+	if IsValid(v) then
+		return grab.Run("CalcVehicleView", v, ply, view)
+	end
+
+	if IsValid(w) then
+		local wf = w.CalcView
+	
+		if wf then
+			view.origin, view.angles, view.fov = wf(w, ply, pos * 1, angles * 1, fov)
+		end
+	end
+
+	return view
+end)
+
+grab.Add("Think", tostring({}), function()
+	if vars["antiblind"] then
+		for k, v in pairs(detours.hooktable) do
+			if k ~= "HUDPaint" then
+				continue
+			end
+
+			for j, _ in pairs(v) do
+				if j == "ulx_blind" then
+					alertDetour("ulx blind")
+
+					grab.Remove("HUDPaint", "ulx_blind")
+				end
+			end
+		end
+	end
+end)
+
+grab.Add("RenderScene", tostring({}), function()
+	if vars["fullbright"] then
+		for _, v in ipairs(game.GetWorld():GetMaterials()) do
+			Material(v):SetVector("$color", Vector(1, 1, 1))
+		end
+
+		render.SuppressEngineLighting(false)
+		render.ResetModelLighting(1, 1, 1)
+
+		render.SetLightingMode(1)
+	else
+		render.SetLightingMode(0)
+	end
+end)
+
+grab.Add("PostDrawViewModel", tostring({}), function(viewmodel)
+	if not viewmodel then
+		return
+	end
+
+	render.SetLightingMode(0)
+
+	for k, _ in ipairs(viewmodel:GetMaterials()) do
+		render.MaterialOverrideByIndex(k - 1, nil)
+	end
+end)
+
+grab.Add("PreDrawEffects", tostring({}), function()
+	render.SetLightingMode(0)
+end)
+
+--[[
+	Commands
+]]
+
+cmd.Add("m_render_fov", function(ply, c, args)
+	args[1] = math.Clamp(args[1], 0, 360)
+
+	vars["fov"] = args[1] or 120
+end)
+
+cmd.Add("m_render_toggle_fullbright", function()
+	vars["fullbright"] = !vars["fullbright"]
+end)
+
+cmd.Add("m_render_toggle_antiblind", function()
+	vars["antiblind"] = !vars["antiblind"]
+end)
+
+cmd.Add("m_tools_gestureloop_set", function(ply, c, args)
+	vars["gesture"] = args[1] or "dance"
+end)
+
+cmd.Add("m_tools_psay_message", function(ply, c, args)
+	vars["psay_msg"] = args[1] or "message"
+end)
+
+cmd.Add("m_tools_toggle_gestureloop", function()
+	vars["gestureloop"] = !vars["gestureloop"]
+
+	if vars["gesture"] then
+		timer.Create(vars["fasttimer"], 0.1, 0, function()
+			detours.runconsolecommand("act", vars["gesture"])
+		end)
+	else
+		timer.Remove(vars["fasttimer"])
+	end
+end)
+
+cmd.Add("m_tools_toggle_psay", function()
+	vars["psay"] = !vars["psay"]
+
+	if vars["psay"] then
+		timer.Create(vars["slowtimer"], 1, 0, function()
+			for _, v in ipairs(player.GetAll()) do
+				if v ~= LocalPlayer and v:IsValid() then
+					detours.runconsolecommand("ulx", "psay", v:Name(), vars["psay_msg"])
+				end
+			end
+		end)
+	else
+		timer.Remove(vars["slowtimer"])
+	end
+end)
