9c808d894cee9d4f98c44e4be53e3b3c6a791e85
ts03GCZqIsTZtu4 <24661212+ts03GCZqIsTZtu4@users.noreply.github.com>
2021-07-26 00:56:43 -0400

    Update swag_tools_beta.lua
    d321ewfds

--- swag_tools_beta.lua
+++ swag_tools_beta.lua
@@ -608,7 +608,7 @@ hook.Add("CreateMove", vars["hookname"], function(cmd)
 			local lposnz = Vector(lpos.x, lpos.y, 0)
 			local tposnz = Vector(tpos.x, tpos.y, 0)
 
-			local tvel = math.Clamp(meta_vc.Length2D(meta_en.GetVelocity(tply)), 450, meta_pl.GetMaxSpeed(LocalPlayer()))
+			local tvel = math.Clamp(meta_vc.Length2D(meta_en.GetVelocity(tply)) * 10, 0, meta_pl.GetMaxSpeed(LocalPlayer()))
 			local dir = tpos - lpos
 			local dis = meta_vc.Distance(lposnz, tposnz)
 			local mvec = Vector(dir.x, dir.y, 0)
@@ -616,7 +616,7 @@ hook.Add("CreateMove", vars["hookname"], function(cmd)
 
 			local yaw = math.rad(ang.y - lang.y)
 
-			if dis > 50 and not meta_cd.KeyDown(cmd, IN_SPEED) and not meta_pl.Crouching(tply) then
+			if not meta_cd.KeyDown(cmd, IN_SPEED) and not meta_pl.Crouching(tply) then
 				meta_cd.SetButtons(cmd, meta_cd.GetButtons(cmd) + IN_SPEED)
 			end
 	
