f0fe0b4fe3c3ba6525d0e7272d96b16ae379e205
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2021-11-20 20:30:37 -0500

    Update swag_tools_loader.lua
    better localization
    works better in meth autorun

--- swag_tools_loader.lua
+++ swag_tools_loader.lua
@@ -1,43 +1,80 @@
-local table = table.Copy(table)
-
-local Color = Color
-local http = table.Copy(http)
-local jit = table.Copy(jit)
-local MsgC = MsgC
-local RunString = RunString
-local surface = table.Copy(surface)
+-- Meth stuff
 
 local mrend, mutil
 
 if meth_lua_api then
-	if meth_lua_api.render then
-		mrend = meth_lua_api.render
-	end
-
-	if meth_lua_api.util then
-		mutil = meth_lua_api.util
-	end
+	mrend = meth_lua_api.render
+	mutil = meth_lua_api.util
 end
 
-http.Fetch("https://raw.githubusercontent.com/awesomeusername69420/meth_tools/main/swag_tools.lua",
-	function(b)
-		if mutil then
-			mutil.RunString(b)
-		else
-			RunString(b)
-		end
-	end,
+-- Localization
 
-	function(e)
-		if mrend then
-			mrend.PushAlert("Failed to load Swag Tools.")
-			mrend.PushAlert("Reason: " .. e)
-		else
-			surface.PlaySound("buttons/button10.wav")
+local table = table.Copy(table)
 
-			MsgC(Color(255, 100, 100), "[$W467001Z] ", Color(222, 222, 222), "Failed to load Swag Tools. Reason: ", Color(255, 100, 100), e .. "\n")
+local debug = table.Copy(debug)
+local pairs = pairs
+local type = type
+
+local function tCopy(n, t)
+	if not n then
+		return nil
+	end
+	
+	local c = {}
+	
+	debug.setmetatable(c, debug.getmetatable(n))
+	
+	for k, v in pairs(n) do
+		if type(v) ~= "table" then
+			c[k] = v
+		else
+			t = t or {}
+			t[n] = c
+			
+			if t[v] then
+				c[k] = t[v]
+			else
+				c[k] = tCopy(v, t)
+			end
 		end
 	end
-)
+	
+	return c
+end
 
-jit.flush()
+local Color = Color
+local http = tCopy(http)
+local LocalPlayer = LocalPlayer
+local math = tCopy(math)
+local MsgC = MsgC
+local RunString = RunString
+local string = tCopy(string)
+local surface = tCopy(surface)
+local timer = tCopy(timer)
+local tostring = tostring
+
+local meta_en = tCopy(debug.getregistry()["Entity"])
+
+local INIT_TIMER_NAME = string.char(math.random(97, 122)) .. tostring(math.random(-123456, 123456))
+
+timer.Create(INIT_TIMER_NAME, 1, 0, function()
+	if meta_en.IsValid(LocalPlayer()) then -- Wait until LocalPlayer is valid before loading
+		http.Fetch("https://raw.githubusercontent.com/awesomeusername69420/meth_tools/main/swag_tools.lua", function(b)
+			if mutil then
+				mutil.RunString(b)
+			else
+				RunString(b)
+			end
+		end, function(e)
+			if mrend then
+				mrend.PushAlert("Failed to load Swag Tools.")
+				mrend.PushAlert("Reason: " .. e)
+			else
+				surface.PlaySound("buttons/button10.wav")
+				MsgC(Color(255, 100, 100), "[STL] ", Color(222, 222, 222), "Failed to load Swag Tools. Reason: ", Color(255, 100, 100), e .. "\n")
+			end
+		end)
+		
+		timer.Remove(INIT_TIMER_NAME)
+	end
+end)
