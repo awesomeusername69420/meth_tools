56392e6783390ddd00e3336e8789605f6311aa79
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2021-12-19 15:41:39 -0500

    Add files via upload
    welcome back

---
+++ other/libbyhacks.lua
@@ -0,0 +1,99 @@
+--[[
+	https://github.com/awesomeusername69420/meth_tools
+	
+	Todo:
+		Auto detect minimum and maximum enterable values
+]]
+
+if meth_lua_api then
+	mutil = meth_lua_api.util
+end
+
+local stor = {
+	["group"] = nil,
+	["canhp"] = false,
+	["maxhp"] = 500,
+	["canarmor"] = false,
+	["maxarmor"] = 255,
+	["canammo"] = false,
+	["maxammo"] = 100,
+}
+
+local function hasAccessToCommand(cmd) -- Tests if the player can run a command
+	if ULib then
+		if ULib.ucl then
+			if ULib.ucl.query then
+				local access, accessTag = ULib.ucl.query(LocalPlayer(), cmd)
+				
+				return access or false
+			end
+		end
+	end
+	
+	return false
+end
+
+timer.Create(tostring({}), 1, 0, function()
+	local lname = LocalPlayer():GetName()
+
+	-- Godmode
+
+	if stor.canhp then
+		if LocalPlayer():Health() < stor.maxhp then
+			LocalPlayer():ConCommand("ulx hp \"" .. lname .. "\" " .. stor.maxhp)
+		end
+	end
+
+	if stor.canarmor then
+		if LocalPlayer():Armor() < stor.maxarmor then
+			LocalPlayer():ConCommand("ulx armor \"" .. lname .. "\" " .. stor.maxarmor)
+		end
+	end
+	
+	-- Infinite ammo
+	
+	if stor.canammo then
+		local wep = LocalPlayer():GetActiveWeapon()
+		
+		if IsValid(wep) then
+			local ammotype = wep:GetPrimaryAmmoType()
+			
+			if ammotype ~= -1 then
+				if  LocalPlayer():GetAmmoCount(ammotype) < stor.maxammo then
+					LocalPlayer():ConCommand("ulx giveammo \"" .. lname .. "\" " .. stor.maxammo)
+				end
+			end
+		end
+	end
+end)
+
+-- Auto add/remove build mode people to friends
+
+hook.Add("Tick", tostring({}), function()
+	if mutil then
+		for _, v in ipairs(player.GetAll()) do
+			local ind = v:EntIndex()
+		
+			if v:GetNWBool("buildmode") then
+				mutil.AddFriend(ind)
+			else
+				if mutil.IsFriend(ind) then
+					mutil.DelFriend(ind)
+				end
+			end
+		end
+	end
+	
+	if stor.group ~= LocalPlayer():GetUserGroup() then
+		stor.canhp = hasAccessToCommand("ulx hp")
+		stor.canarmor = hasAccessToCommand("ulx armor")
+		stor.canammo = hasAccessToCommand("ulx giveammo")
+	
+		-- Will exist later
+		--stor.maxhp = getMaxInt("ulx hp")
+		--stor.maxarmor = getMaxInt("ulx armor")
+		--stor.maxammo = getMaxInt("ulx giveammo")
+	
+		stor.group = LocalPlayer():GetUserGroup()
+	end
+end)
