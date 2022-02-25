df44d794aa1d541905fc56b8652a973c1092cf77
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-02-25 17:23:22 -0500

    Update swastika_crosshair.lua
    added DrawOverlay alternative to non meth havers

--- other/swastika_crosshair.lua
+++ other/swastika_crosshair.lua
@@ -19,34 +19,68 @@ local function canRender()
 	return not vgui.CursorVisible() and not gui.IsConsoleVisible() and not gui.IsGameUIVisible() and not LocalPlayer():IsTyping()
 end
 
-meth_lua_api.callbacks.Add("OnHUDPaint", "", function()
-	if not canRender() then
-		return
-	end
-
-	local length = stuff.convars.length:GetFloat()
-	local center = stuff.center
-	local x, y = center.x, center.y
-
-	local ogrt = render.GetRenderTarget()
-	render.SetRenderTarget()
-
-	draw.NoTexture()
-	surface.SetDrawColor(stuff.color_red)
+if meth_lua_api then
+	meth_lua_api.callbacks.Add("OnHUDPaint", "", function()
+		if not canRender() then
+			return
+		end
 	
-	local matrix = Matrix()
-	matrix:Translate(center)
-	matrix:Rotate(Angle(0, math.NormalizeAngle(SysTime() * (stuff.convars.spin:GetFloat() * 10)), 0))
-	matrix:Translate(-center)
-
-	cam.PushModelMatrix(matrix)
-		surface.DrawLine(x, y - length, x, y + length)
-		surface.DrawLine(x - length, y, x + length, y)
-		surface.DrawLine(x, y - length, x + length, y - length)
-		surface.DrawLine(x, y + length, x - length, y + length)
-		surface.DrawLine(x - length, y, x - length, y - length)
-		surface.DrawLine(x + length, y, x + length, y + length)
-	cam.PopModelMatrix()
-
-	render.SetRenderTarget(ogrt)
-end)
+		local length = stuff.convars.length:GetFloat()
+		local center = stuff.center
+		local x, y = center.x, center.y
+	
+		local ogrt = render.GetRenderTarget()
+		render.SetRenderTarget()
+	
+		draw.NoTexture()
+		surface.SetDrawColor(stuff.color_red)
+		
+		local matrix = Matrix()
+		matrix:Translate(center)
+		matrix:Rotate(Angle(0, math.NormalizeAngle(SysTime() * (stuff.convars.spin:GetFloat() * 10)), 0))
+		matrix:Translate(-center)
+	
+		cam.PushModelMatrix(matrix)
+			surface.DrawLine(x, y - length, x, y + length)
+			surface.DrawLine(x - length, y, x + length, y)
+			surface.DrawLine(x, y - length, x + length, y - length)
+			surface.DrawLine(x, y + length, x - length, y + length)
+			surface.DrawLine(x - length, y, x - length, y - length)
+			surface.DrawLine(x + length, y, x + length, y + length)
+		cam.PopModelMatrix()
+	
+		render.SetRenderTarget(ogrt)
+	end)
+else
+	hook.Add("DrawOverlay", "", function()
+		if not canRender() then
+			return
+		end
+	
+		local length = stuff.convars.length:GetFloat()
+		local center = stuff.center
+		local x, y = center.x, center.y
+	
+		local ogrt = render.GetRenderTarget()
+		render.SetRenderTarget()
+	
+		draw.NoTexture()
+		surface.SetDrawColor(stuff.color_red)
+		
+		local matrix = Matrix()
+		matrix:Translate(center)
+		matrix:Rotate(Angle(0, math.NormalizeAngle(SysTime() * (stuff.convars.spin:GetFloat() * 10)), 0))
+		matrix:Translate(-center)
+	
+		cam.PushModelMatrix(matrix)
+			surface.DrawLine(x, y - length, x, y + length)
+			surface.DrawLine(x - length, y, x + length, y)
+			surface.DrawLine(x, y - length, x + length, y - length)
+			surface.DrawLine(x, y + length, x - length, y + length)
+			surface.DrawLine(x - length, y, x - length, y - length)
+			surface.DrawLine(x + length, y, x + length, y + length)
+		cam.PopModelMatrix()
+	
+		render.SetRenderTarget(ogrt)
+	end)
+end
