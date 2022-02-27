d544be9b798b3cc61d1e3a9cb14eb0f32fc6de68
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-02-27 17:27:10 -0500

    Update 3d_penetration_crosshair.lua
    using CalcView to get EyePos and EyeAngles because meth breaks EyePos fuck you wolfie fuck you fuck you fuck you fuckyou fuckyoufuckyou

--- other/3d_penetration_crosshair.lua
+++ other/3d_penetration_crosshair.lua
@@ -3,6 +3,8 @@
 ]]
 
 local cache = {
+	eyepos = vector_origin,
+	eyeangles = angle_zero,
 	mins = Vector(-4, -4, -4),
 	maxs = Vector(4, 4, 4),
 
@@ -62,8 +64,8 @@ end
 
 local function getEyeTrace()
 	local tr = util.TraceLine({
-		start = EyePos(),
-		endpos = EyePos() + (EyeAngles():Forward() * 32768),
+		start = cache.eyepos,
+		endpos = cache.eyepos + (cache.eyeangles:Forward() * 32768),
 		mask = MASK_SHOT,
 		filter = LocalPlayer()
 	})
@@ -222,12 +224,12 @@ local function canPenetrate()
 		end
 
 		local eyepos = eyetrace.HitPos
-		local forward = EyeAngles():Forward()
+		local forward = cache.eyeangles:Forward()
 		local endtrace = nil
 		local endpos = nil
 
 		for i = 1, 75 do -- There's probably a better way of doing this but this is what I came up with so fuck you
-			local cur = eyepos + (forward * i)
+			local cur = cache.eyepos + (forward * i)
 
 			local tr = util.TraceLine({
 				start = cur,
@@ -277,7 +279,7 @@ if meth_lua_api then
 		cache.weapon.lastpen = penetrate
 
 		local col = penetrate and cache.colors.green or cache.colors.red
-	
+
 		cam.Start3D()
 			render.DrawBox(endpos, angle_zero, cache.mins, cache.maxs, col)
 			render.DrawWireframeBox(endpos, angle_zero, cache.mins, cache.maxs, col)
@@ -308,3 +310,12 @@ else
 		render.SetRenderTarget(ogrt)
 	end)
 end
+
+hook.Add("CalcView", tostring({}), function(ply, pos, ang)
+	if not IsValid(ply) then
+		return
+	end
+
+	cache.eyepos = pos
+	cache.eyeangles = ang
+end)
